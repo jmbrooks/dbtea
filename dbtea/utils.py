5b380e57d65b35a48724f5f20ffbba8f262cedca
Johnathan Brooks <jb@4mile.io>
2021-04-13 21:12:24 -0700

    Add run CLI and parse JSON utils

--- dbtea/utils.py
+++ dbtea/utils.py
@@ -1,5 +1,11 @@
+import json
 import os
+import subprocess
+import sys
+
 import yaml
+
+from typing import List, Union
 
 from dbtea.exceptions import DbteaException
 from dbtea.logger import DBTEA_LOGGER as logger
@@ -51,14 +57,49 @@ def fetch_dbt_project_directory(custom_project_directory: str = None) -> str:
     )
 
 
-def parse_yaml_file(yaml_file: str) -> dict:
+def flatten_json(json_data: dict, delimiter: str = "_"):
+    """"""
+    flattened_output = {}
+    for key in json_data.keys():
+        if isinstance(json_data[key], dict):
+            get = flatten_json(json_data[key], delimiter)
+            for sub_key in get.keys():
+                flattened_output[key + delimiter + sub_key] = get[sub_key]
+        else:
+            flattened_output[key] = json_data[key]
+
+    return flattened_output
+
+
+def parse_yaml_file(yaml_file_path: str) -> dict:
     """Parse dbt config YAML file to Python dictionary."""
-    with open(yaml_file, "r") as yaml_stream:
-        yaml_data = yaml.safe_load(yaml_stream)
+    with open(yaml_file_path, "r") as yaml_stream:
+        yaml_data = yaml.safe_load(yaml_stream) or {}
 
     return yaml_data
 
 
-def compare_configs(config_schema: dict, config_schema2: dict) -> dict:
-    """Parse dbt config YAML file to Python dictionary."""
-    pass
+def parse_json_file(json_file_path: str) -> dict:
+    """Parse JSON file to Python dictionary."""
+    with open(json_file_path, "r") as json_stream:
+        json_data = json.load(json_stream)
+
+    return json_data
+
+
+def run_cli_command(command: Union[List[str], str], working_directory: str, use_shell: bool = False,
+                    output_as_text: bool = True, capture_output: bool = True, **kwargs):
+    """Execute command line subprocess"""
+    result = subprocess.run(command, shell=use_shell, cwd=working_directory, text=output_as_text,
+                            capture_output=capture_output, **kwargs)
+
+    if result.stderr:
+        raise subprocess.CalledProcessError(returncode=result.returncode, cmd=result.args, stderr=result.stderr)
+    if result.stdout:
+        if "Encountered an error" in result.stdout:  # Handle for dbt stdout errors
+            logger.error("dbt Error: {}".format(result.stdout))
+            raise subprocess.CalledProcessError(returncode=result.returncode, cmd=result.args, stderr=result.stdout)
+
+        logger.debug("Command Result:\n{}".format(result.stdout))
+
+    return result.stdout
