f2f98db0acd2580f3aa66f0def78ec73f06729a8
Johnathan Brooks <jb@4mile.io>
2021-04-17 13:54:51 -0700

    Added file existence checks for file parsing

--- dbtea/utils.py
+++ dbtea/utils.py
@@ -1,15 +1,28 @@
+import functools
 import json
 import os
 import subprocess
+import timeit
 
 import yaml
 
-from typing import List, Union
+from pathlib import Path
+from typing import Callable, List, Union
 
 from dbtea.exceptions import DbteaException
 from dbtea.logger import DBTEA_LOGGER as logger
 
 DBT_PROJECT_FILE = "dbt_project.yml"
+
+
+def assemble_path(*args):
+    """"""
+    return os.path.join(*args)
+
+
+def file_exists(file_path: str) -> bool:
+    """"""
+    return Path(file_path).is_file()
 
 
 def fetch_dbt_project_directory(custom_project_directory: str = None) -> str:
@@ -23,9 +36,7 @@ def fetch_dbt_project_directory(custom_project_directory: str = None) -> str:
     root_path = os.path.abspath(os.sep)
 
     if custom_project_directory:
-        custom_directory_project_file = os.path.join(
-            custom_project_directory, DBT_PROJECT_FILE
-        )
+        custom_directory_project_file = assemble_path(custom_project_directory, DBT_PROJECT_FILE)
         if os.path.exists(custom_directory_project_file):
             return custom_project_directory
         else:
@@ -37,7 +48,7 @@ def fetch_dbt_project_directory(custom_project_directory: str = None) -> str:
             )
 
     while project_directory != root_path:
-        dbt_project_file = os.path.join(project_directory, DBT_PROJECT_FILE)
+        dbt_project_file = assemble_path(project_directory, DBT_PROJECT_FILE)
         if os.path.exists(dbt_project_file):
             logger.info(
                 "Running dbtea against dbt project at path: {}".format(
@@ -70,13 +81,53 @@ def flatten_json(json_data: dict, delimiter: str = "_"):
     return flattened_output
 
 
-def assemble_path(*args):
+def get_detail(fn_name: str):
     """"""
-    return os.path.join(*args)
+    detail_map = {
+        "run_sql": "SQL ",
+        "run_assert": "data test ",
+        "run_content": "content ",
+    }
+    return detail_map.get(fn_name, "")
+
+
+def human_readable(elapsed: float):
+    """"""
+    minutes, seconds = divmod(elapsed, 60)
+    elapsed_minutes = f"{minutes:.0f} minute{'s' if minutes > 1 else ''}"
+    elapsed_seconds = f"{seconds:.0f} second{'s' if round(seconds) != 1 else ''}"
+    time_separator = " and " if seconds and minutes else ""
+
+    return f"{elapsed_minutes if minutes else ''}{time_separator}{elapsed_seconds if seconds else ''}"
+
+
+def log_duration(fn: Callable):
+    """"""
+    functools.wraps(fn)
+
+    def timed_function(*args, **kwargs):
+        start_time = timeit.default_timer()
+        try:
+            result = fn(*args, **kwargs)
+        finally:
+            elapsed_time = timeit.default_timer() - start_time
+            elapsed_formatted = human_readable(elapsed_time)
+            message_detail = get_detail(fn.__name__)
+
+            logger.info(f"Completed {message_detail}operation in {elapsed_formatted}.\n")
+        return result
+
+    return timed_function
 
 
 def parse_yaml_file(yaml_file_path: str) -> dict:
     """Parse dbt config YAML file to Python dictionary."""
+    if not file_exists(yaml_file_path):
+        raise DbteaException(
+            name="missing-yaml-file",
+            title="YAML file set to parse is missing",
+            detail="Attempted to parse YAML file at path {}, however this path is not a file".format(yaml_file_path)
+        )
     with open(yaml_file_path, "r") as yaml_stream:
         yaml_data = yaml.safe_load(yaml_stream) or {}
 
@@ -85,6 +136,12 @@ def parse_yaml_file(yaml_file_path: str) -> dict:
 
 def parse_json_file(json_file_path: str) -> dict:
     """Parse JSON file to Python dictionary."""
+    if not file_exists(json_file_path):
+        raise DbteaException(
+            name="missing-json-file",
+            title="JSON file set to parse is missing",
+            detail="Attempted to parse JSON file at path {}, however this path is not a file".format(json_file_path)
+        )
     with open(json_file_path, "r") as json_stream:
         json_data = json.load(json_stream)
 
@@ -107,3 +164,9 @@ def run_cli_command(command: Union[List[str], str], working_directory: str, use_
         logger.debug("Command Result:\n{}".format(result.stdout))
 
     return result.stdout
+
+
+def write_to_yaml_file(data: dict, yaml_file_path: str) -> None:
+    """Parse dbt config YAML file to Python dictionary."""
+    with open(yaml_file_path, "w") as yaml_stream:
+        yaml.safe_dump(data, yaml_stream)
