43cc90bfb58510efcf409a9e1471d773b72bf775
Johnathan Brooks <jb@4mile.io>
2021-05-16 13:34:40 -0700

    Add config file processing for dbtea configurable settings

--- dbtea/config.py
+++ dbtea/config.py
@@ -1,7 +1,9 @@
+from configparser import ConfigParser
 from pathlib import Path
+from typing import Optional
 
 import dbtea.utils as utils
-from dbtea.dbt_client.project import PROFILES_DIR
+from dbtea.clients.dbt import PROFILES_DIR
 from dbtea.exceptions import DbteaException
 from dbtea.logger import DBTEA_LOGGER as logger
 
@@ -17,10 +19,27 @@ class DbteaConfig:
         config_dir: str = PROFILES_DIR,
         config_data: dict = None,
         replace_config_if_exists: bool = True,
+        dbt_project: Optional[str] = None,
+        looker_project: Optional[str] = None,
+        looker_config_path: Optional[str] = None,
+        looker_config_section: Optional[str] = None,
+        looker_replace_config_if_exists: Optional[bool] = True
     ):
         self.file_name = file_name
         self.config_dir = config_dir
-        self.config_data = config_data
+        self.dbt_project = dbt_project
+        self.looker_project = looker_project
+        self.looker_config_path = looker_config_path or utils.assemble_path(utils.get_home_dir(), ".dbt", "looker.ini")
+        self.looker_config_section = looker_config_section or "looker"
+        self.looker_config = ConfigParser() if looker_project else None
+
+        if self._config_data_exists() and not replace_config_if_exists:
+            self.config_data = utils.parse_yaml_file(self.config_name_and_path)
+        else:
+            self.config_data = config_data or {}
+
+        if looker_project and not self._config_file_exists(self.looker_config_path) or looker_replace_config_if_exists:
+            self.write_looker_config()
 
         self.write_data_to_file(replace_if_exists=replace_config_if_exists)
 
@@ -59,23 +78,32 @@ class DbteaConfig:
 
     def write_data_to_file(self, replace_if_exists: bool = True) -> None:
         """"""
-        if (
-            self._config_file_exists()
-            and self._config_data_exists()
-            and not replace_if_exists
-        ):
-            logger.warning(
-                "Dbtea config file already exists, ignoring write config data to file step"
-            )
+        if self._config_file_exists() and self._config_data_exists() and not replace_if_exists:
+            logger.warning("Dbtea config file already exists, ignoring write config data to file step")
         else:
-            logger.info(
-                "Creating {} file at path: {}".format(self.file_name, self.config_dir)
-            )
+            logger.info("Creating {} file at path: {}".format(self.file_name, self.config_dir))
             utils.write_to_yaml_file(self.config_data, self.config_name_and_path)
 
-    def _config_file_exists(self) -> bool:
-        return utils.file_exists(self.config_name_and_path)
+    def read_looker_config(self):
+        """"""
+        return ConfigParser().read(self.looker_config_path)
+
+    def write_looker_config(self):
+        """"""
+        logger.info("Writing Looker config file at path: {}".format(self.looker_config_path))
+        self.looker_config.add_section(self.looker_config_section)
+        for key, value in self.config_data.get(self.dbt_project, {}).items():
+            if key.startswith("looker_sdk_"):
+                self.looker_config.set(self.looker_config_section, str(key.replace("looker_sdk_", "")), str(value))
+
+        with open(self.looker_config_path, "w") as config_stream:
+            self.looker_config.write(config_stream)
+
+    def _config_file_exists(self, file_path: Optional[str] = None) -> bool:
+        """"""
+        return utils.file_exists(file_path or self.config_name_and_path)
 
     def _config_data_exists(self) -> bool:
+        """"""
         config_data = utils.parse_yaml_file(self.config_name_and_path)
         return True if config_data else False
